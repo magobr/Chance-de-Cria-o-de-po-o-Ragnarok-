5d93da6257861beaca1cd27ee7d6f974d2f2fe1b
Thiago Novaes Sampaio dos Santos <novaesthago@gmail.com>
2018-05-07 17:00:06 -0300

    Versão inicial

---
+++ Caucula Sorte/Caucula Sorte/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Caucula_Sorte
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*pp - Pesquisa de poçoes
+              PR - Preparar Poçoes
+              MP - Mudança de Planos
+              NC - Nivel de classe
+              DZ - Destreza
+              ST - Sorte
+              IT - Inteligencia
+              PC - Poção
+             */
+            double
+            PP,
+            PR,
+            MP,
+            NC,
+            DZ,
+            ST,
+            IT,
+            PC;
+
+            Console.WriteLine("\t\t***Bem Vindo a calculadora de poções***\n\n");
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
